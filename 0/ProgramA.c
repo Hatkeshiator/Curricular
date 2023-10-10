326eac1db80fbab7fae0bf2221056fb3bfe65db2
Varad K. Dixit <143983431+Hatkeshiator@users.noreply.github.com>
2023-10-10 13:05:39 +0530

    Delete 0/ProgramA.c
    Now in dir for first practical

--- 0/ProgramA.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(void) {
-    system("clear");
-    printf("My name is Varad K. Dixit\nI live on house no.s 55-56 Sunrise Tenements, on Thaltej road,\nin Ahmedabad city, PIN code 380054");
-    getchar();
-    return 0;
-}
