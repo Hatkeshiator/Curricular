367b2743ef2dbf8097fe0285153fe31e25197ef0
Varad Dixit <143983431+Hatkeshiator@users.noreply.github.com>
2024-02-14 13:37:01 +0530

    Create 15triarea.py
    turns out sleep deprived me relies on AI. no more all nighters for this guy

---
+++ pythonlab/15triarea.py
@@ -0,0 +1,13 @@
+# Write a program to check the area of a triangle given the cartesian coordinates of its vertices
+
+def triangle_area(x_1,y_1,x_2,y_2,x_3,y_3):
+    # imagine copying the triangle you have and rotating it by 180 degrees
+    # now put the like sides of the triangle on each other
+    # this resultant shape is a parallelogram
+    # the formula for the area of a paralellogram given three of its points is:
+    area = (x_1-x_3)*(y_2-y_1) - (x_1-x_2)*(y_3-y_1)
+    # but this value may be negative for certain coordinates, so we take modulus:
+    area = abs(area)
+    # and the triangle is, by definition, half of the parallelogram
+    area /= 2
+    return area
