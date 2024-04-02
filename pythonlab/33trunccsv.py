9af55282895e84665edb093d21769dbc02909c5a
Varad Dixit <23BBT035@spt.pdpu.ac.in>
2024-04-02 13:33:54 +0530

    Added problem statement to program 33.

--- pythonlab/33trunccsv.py
+++ pythonlab/33trunccsv.py
@@ -1,3 +1,5 @@
+# Write a program to read only columns with specific headers from a csv files
+
 import csv
 
 columns_to_read = input("What are the headings of the columns you want to read? ").split()
